f050de73fd0b90f794f52000a1e7df3c5248539f
JAEX4U <joydeephembram89@gmail.com>
2026-03-19 22:09:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const products = [
-  {name:'Amazon $10', subtitle:'Instant Delivery • USA', price:'$10', category:'D'},
-  {name:'Steam $20', subtitle:'Global Key • Fast', price:'$20', category:'C'},
-  {name:'Google Play $25', subtitle:'Secure Delivery', price:'$25', category:'D'},
+  {name:'Amazon $10', subtitle:'Instant Delivery • USA', price:'$10', detail:'ZIP', category:'D'},
+  {name:'Steam $20', subtitle:'Global Key • Fast', price:'$20', detail:'ZIP', category:'C'},
+  {name:'Google Play $25', subtitle:'Secure Delivery', price:'$25', detail:'ZIP', category:'D'},
   {name:'IDAHO CENTRAL CREDIT UNION', subtitle:'APP,Instant minics, Alerts, Travel center, SSN+DOB', price:'$50', detail:'ZIP', category:'C'},
-  {name:'VISA Primium', subtitle:'AU | BALANCE= $100', price:'$30', category:'D'},
+  {name:'VISA Primium', subtitle:'AU | BALANCE= $100', price:'$30', detail:'ZIP', category:'D'},
   
-  {name:'NFC Basic', subtitle:'Tap Enabled', price:'$30', category:'NFC'},
-  {name:'NFC Pro', subtitle:'High Security', price:'$45', category:'NFC'},
-  {name:'Google Play $25', subtitle:'Secure Delivery', price:'$25', category:'D'},
+  {name:'NFC Basic', subtitle:'Tap Enabled', price:'$30', detail:'ZIP', category:'NFC'},
+  {name:'NFC Pro', subtitle:'High Security', price:'$45', detail:'ZIP', category:'NFC'},
+  {name:'Google Play $25', subtitle:'Secure Delivery', price:'$25', detail:'ZIP', category:'D'},
   
-  {name:'OTP Basic', subtitle:'SMS Verified', price:'$15', category:'OTP'},
-  {name:'OTP Pro', subtitle:'High Success', price:'$25', category:'OTP'}
+  {name:'OTP Basic', subtitle:'SMS Verified', price:'$15', detail:'ZIP', category:'OTP'},
+  {name:'OTP Pro', subtitle:'High Success', price:'$25', detail:'ZIP', category:'OTP'}
 ];
 
 function render(f='D'){
